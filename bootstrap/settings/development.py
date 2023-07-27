4f817c287ff2315f2819dd22aa0f3e6111329d2c
mohamed.g.ismail@orange.com <mohamed.g.ismail@orange.com>
2023-07-27 17:59:53 +0300

    fix issues

--- bootstrap/settings/development.py
+++ bootstrap/settings/development.py
@@ -8,10 +8,6 @@ from __future__ import annotations
 from .common import *
 
 DEBUG = convert_str_bool(getenv('DJANGO_DEBUG'), True)
-INSTALLED_APPS += ['silk', ]
-MIDDLEWARE += [
-    'silk.middleware.SilkyMiddleware',
-]
 
 # Disable authentication
 ORANGE_AUTH_API_BYPASS = True
