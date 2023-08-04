4f76f9e1b0deb72085cf87c82567a54b66db73bc
BADAWY Mohammed OBS/GDO <mohammed.badawy@orange.com>
2023-08-04 16:54:52 +0300

    Brand and color dropdown list
    Car registration No. of seats validation to max 3

--- apps/website/forms/CarRegistration.py
+++ apps/website/forms/CarRegistration.py
@@ -3,6 +3,11 @@ from __future__ import annotations
 from django import forms
 from apps.website.models import Car
 from apps.website.models import CarPlate
+
+from apps.website.jsonData import JsonData
+
+
+
 # import  from models.py
 
 # create a ModelForm
@@ -10,6 +15,13 @@ from apps.website.models import CarPlate
 
 class CarRegistrationForm(forms.ModelForm):
     # specify the name of model to use
+    car_brands = JsonData.get_carbrand()
+    Car_Manufacture= forms.CharField(label='Brand', widget=forms.Select(choices=car_brands))
+
+    all_colors = JsonData.get_carcolor()
+    Car_Color= forms.CharField(label='Color', widget=forms.Select(choices=all_colors))
+
+    No_OF_Seats = forms.CharField(initial=3, widget=forms.NumberInput(attrs={'min': '1', 'max': '3'}))
     class Meta:
         model = Car
         fields = [
