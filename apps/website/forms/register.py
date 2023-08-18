ae85c4550cde5c068297578be26c71baaeacd858
mohamed.g.ismail@orange.com <mohamed.g.ismail@orange.com>
2023-08-18 18:15:34 +0300

    add profile pic to register form

--- apps/website/forms/register.py
+++ apps/website/forms/register.py
@@ -1,10 +1,10 @@
 from __future__ import annotations
 
-from apps.website.jsonData import JsonData
 from django.contrib.auth.forms import UserChangeForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.forms import CharField
+from django.forms import ImageField
 from django.forms import HiddenInput
 from django.forms import ModelForm
 from django.forms import Select
@@ -13,112 +13,100 @@ from apps.website.models import Profile
 
 
 class RegisterForm(UserCreationForm):
-
     password1 = CharField(widget=HiddenInput(), required=False)
 
     password2 = CharField(widget=HiddenInput(), required=False)
 
     def __init__(self, *args, request=None, **kwargs):
-
         super(UserCreationForm, self).__init__(*args, **kwargs)
 
-        self.fields['username'].help_text = None
+        self.fields["username"].help_text = None
 
         self.request = request
 
         if self.request:
+            self.user_info = self.request.session.get("user_info")
 
-            self.user_info = self.request.session.get('user_info')
+            self.fields["username"].initial = self.user_info.get("sub")
 
-            self.fields['username'].initial = self.user_info.get('sub')
+            self.fields["email"].initial = self.user_info.get("email")
 
-            self.fields['email'].initial = self.user_info.get('email')
-
-            self.fields['first_name'].initial = self.user_info.get(
-                'given_name',
+            self.fields["first_name"].initial = self.user_info.get(
+                "given_name",
             )
 
-            self.fields['last_name'].initial = self.user_info.get(
-                'family_name',
+            self.fields["last_name"].initial = self.user_info.get(
+                "family_name",
             )
 
-            self.fields['username'].widget.attrs['readonly'] = True
+            self.fields["username"].widget.attrs["readonly"] = True
 
-            self.fields['email'].widget.attrs['readonly'] = True
+            self.fields["email"].widget.attrs["readonly"] = True
 
-            self.fields['first_name'].widget.attrs['readonly'] = True
+            self.fields["first_name"].widget.attrs["readonly"] = True
 
-            self.fields['last_name'].widget.attrs['readonly'] = True
+            self.fields["last_name"].widget.attrs["readonly"] = True
 
     class Meta:
-
         model = User
 
-        fields = ['username', 'email', 'first_name', 'last_name']
+        fields = ["username", "email", "first_name", "last_name"]
 
 
 class ChangeUserForm(UserChangeForm):
-
     password1 = CharField(widget=HiddenInput(), required=False)
 
     password2 = CharField(widget=HiddenInput(), required=False)
 
     def __init__(self, *args, **kwargs):
-
         super(UserChangeForm, self).__init__(*args, **kwargs)
 
-        del self.fields['password']
+        del self.fields["password"]
 
-        self.fields['username'].help_text = None
+        self.fields["username"].help_text = None
 
-        self.fields['username'].widget.attrs['readonly'] = True
+        self.fields["username"].widget.attrs["readonly"] = True
 
-        self.fields['email'].widget.attrs['readonly'] = True
+        self.fields["email"].widget.attrs["readonly"] = True
 
-        self.fields['first_name'].widget.attrs['readonly'] = True
+        self.fields["first_name"].widget.attrs["readonly"] = True
 
-        self.fields['last_name'].widget.attrs['readonly'] = True
+        self.fields["last_name"].widget.attrs["readonly"] = True
 
     class Meta:
-
         model = User
 
-        fields = ['username', 'email', 'first_name', 'last_name']
+        fields = ["username", "email", "first_name", "last_name"]
 
 
 class ProfileForm(ModelForm):
- 
     phone = PhoneNumberField()
 
     gender = CharField(widget=Select(choices=Profile.UserGender))
 
-    city = CharField(required=True,
-
+    city = CharField(
+        required=True,
         widget=Select(
-            choices=[('', 'Choose City')],
-
-            attrs={'onchange': 'change_areas();', 'id': 'cities'},
+            choices=[("", "Choose City")],
+            attrs={"onchange": "change_areas();", "id": "cities"},
         ),
     )
 
-    area = CharField(required=True,
-
+    area = CharField(
+        required=True,
         widget=Select(
-
-            choices=[('', 'Choose Area')],
-
-            attrs={'id': 'areas'},
+            choices=[("", "Choose Area")],
+            attrs={"id": "areas"},
         ),
-
     )
+    profile_picture = ImageField()
+
     def __init__(self, *args, **kwargs):
-
         super(ModelForm, self).__init__(*args, **kwargs)
 
-        self.fields['phone'].widget.attrs['autocomplete'] = "off"
+        self.fields["phone"].widget.attrs["autocomplete"] = "off"
 
     class Meta:
-
         model = Profile
 
-        fields = ['phone', 'gender', 'city', 'area']
+        fields = ["phone", "gender", "city", "area"]
--- apps/website/views/auth.py
+++ apps/website/views/auth.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+
 from apps.website.jsonData import JsonData
 from django.contrib import messages
 from django.contrib.auth import authenticate
@@ -10,6 +12,9 @@ from django.views.generic import View
 from apps.website.forms.register import ChangeUserForm
 from apps.website.forms.register import ProfileForm
 from apps.website.forms.register import RegisterForm
+from django.core.files.base import ContentFile
+
+from lib.s3_storage.s3_helpers import create_s3_client
 
 
 class AuthView(View):
@@ -17,38 +22,54 @@ class AuthView(View):
 
     def register(request):
         context = {}
-        context['areas'] = JsonData.get_areas()
-        context['cities'] = JsonData.get_cities_json()
-        context['context'] = 'create'
-        if request.method == 'GET':
-            context['user_form'] = RegisterForm(request=request)
-            context['profile_form'] = ProfileForm()
-            return render(request, 'register.html', context)
+        context["areas"] = JsonData.get_areas()
+        context["cities"] = JsonData.get_cities_json()
+        context["context"] = "create"
+        if request.method == "GET":
+            context["user_form"] = RegisterForm(request=request)
+            context["profile_form"] = ProfileForm()
+            return render(request, "register.html", context)
 
-        if request.method == 'POST':
+        if request.method == "POST":
             user_form = RegisterForm(request.POST)
-            profile_form = ProfileForm(request.POST)
+            profile_form = ProfileForm(request.POST, request.FILES)
             if user_form.is_valid() and profile_form.is_valid():
                 user = user_form.save(commit=False)
                 user.save()
                 profile = profile_form.save(commit=False)
                 profile.user = user
-                profile.city = JsonData.get_city_name(request.POST.get('city'))
-                profile.area = JsonData.get_area_name(request.POST.get('area'))
-                profile.save()
-                messages.success(request, 'You have registered successfully.')
-                login(
-                    request, user,
-                    backend='django.contrib.auth.backends.ModelBackend',
+                profile.city = JsonData.get_city_name(request.POST.get("city"))
+                profile.area = JsonData.get_area_name(request.POST.get("area"))
+                # Get img from register form and create s3 client to upload img
+                img = profile_form.cleaned_data.get("profile_picture")
+                client, session = create_s3_client()
+                client.upload_fileobj(
+                    img.open(mode="rb"),
+                    settings.AWS_STORAGE_BUCKET_NAME,
+                    img.name,
                 )
-                return redirect('/')
+                img.close()
+                profile.profile_pic = img.name
+                profile.save()
+                messages.success(request, "You have registered successfully.")
+                login(
+                    request,
+                    user,
+                    backend="django.contrib.auth.backends.ModelBackend",
+                )
+                return redirect("/")
             else:
-                return render(request, 'register.html', {'user_form': user_form,
-                                                          'profile_form': profile_form,
-                                                            'areas':context['areas'],
-                                                            'cities':context['cities'],
-                                                             'context':context['context']
-                                                            })
+                return render(
+                    request,
+                    "register.html",
+                    {
+                        "user_form": user_form,
+                        "profile_form": profile_form,
+                        "areas": context["areas"],
+                        "cities": context["cities"],
+                        "context": context["context"],
+                    },
+                )
 
     def login(request):
         """Implement customized django auth backend with Orange Auth. You can refer to AUTHENTICATION_BACKENDS in django settings.
@@ -69,45 +90,51 @@ class AuthView(View):
             The request object
         """
         logout(request=request)
-        return redirect(request.META.get('HTTP_REFERER', 'pages.home'))
+        return redirect(request.META.get("HTTP_REFERER", "pages.home"))
 
 
 class ProfileView(LoginRequiredMixin, View):
     def edit_profile(request):
         context = {}
-        context['context'] = 'edit'
-        context['areas'] = JsonData.get_areas()
-        context['cities'] = JsonData.get_cities_json()
-        context['user_area'] = request.user.profile.area
-        context['user_city'] = request.user.profile.city
-        if request.method == 'GET':
-            context['user_form'] = ChangeUserForm(instance=request.user)
+        context["context"] = "edit"
+        context["areas"] = JsonData.get_areas()
+        context["cities"] = JsonData.get_cities_json()
+        context["user_area"] = request.user.profile.area
+        context["user_city"] = request.user.profile.city
+        if request.method == "GET":
+            context["user_form"] = ChangeUserForm(instance=request.user)
             profile_form = ProfileForm(instance=request.user.profile)
             profile_form.user = request.user
-            context['profile_form'] = profile_form
-            return render(request, 'register.html', context)
+            context["profile_form"] = profile_form
+            return render(request, "register.html", context)
 
-        if request.method == 'POST':
+        if request.method == "POST":
             user_form = ChangeUserForm(request.POST, instance=request.user)
             profile_form = ProfileForm(
-                request.POST, instance=request.user.profile,
+                request.POST,
+                instance=request.user.profile,
             )
             if user_form.is_valid() and profile_form.is_valid():
                 user = user_form.save(commit=False)
                 user.save()
                 profile = profile_form.save(commit=False)
                 profile.user = user
-                profile.city = JsonData.get_city_name(request.POST.get('city'))
-                profile.area = JsonData.get_area_name(request.POST.get('area'))
+                profile.city = JsonData.get_city_name(request.POST.get("city"))
+                profile.area = JsonData.get_area_name(request.POST.get("area"))
                 profile.save()
-                messages.success(request, 'Edit profile done successfully.')
-                return redirect('/')
+                messages.success(request, "Edit profile done successfully.")
+                return redirect("/")
             else:
-                return render(request, 'register.html', {'user_form': user_form,
-                                                         'profile_form':profile_form,
-                                                            'areas':context['areas'],
-                                                            'cities':context['cities'],
-                                                             'context':context['context'],
-                                                             'user_city':context['user_city'],
-                                                             'user_area':context['user_area'] 
-                                                         })
+                return render(
+                    request,
+                    "register.html",
+                    {
+                        "user_form": user_form,
+                        "profile_form": profile_form,
+                        "areas": context["areas"],
+                        "cities": context["cities"],
+                        "context": context["context"],
+                        "user_city": context["user_city"],
+                        "user_area": context["user_area"],
+                    },
+                )
