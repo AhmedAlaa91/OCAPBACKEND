1ea83e3cfd04ed40823943a72b08bc618d6168b7
vlfx0340 <mahmoud.elsayaad@orange.com>
2023-09-05 10:56:53 +0300

    Replace template_name from "Passengers.html" to "passengers.html"

--- apps/website/views/RidePassengers.py
+++ apps/website/views/RidePassengers.py
@@ -38,7 +38,7 @@ class RequestsView(APIView):
         else:
             content = {"Message": "No passengers requested your ride yet."}
 
-        return Response(content, template_name="Passengers.html")
+        return Response(content, template_name="passengers.html")
 
     def post(self, request, *args, **kwargs):
         result = ""
